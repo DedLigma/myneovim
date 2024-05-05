406ca75e01f359d64f0e0d31ba918331c71589d3
dedligma <dedligma@192.168.1.3>
2024-05-05 03:20:43 +0300

    upd

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,6 @@
-local ui_plugins = require("plugins.ui_plugins")
-local git_plugins = require("plugins.git_plugins")
-local base_code_plugins = require("plugins.base_code_plugins")
+local ui_plugins = require("plugins_lazy.ui_plugins")
+local git_plugins = require("plugins_lazy.git_plugins")
+local base_code_plugins = require("plugins_lazy.base_code_plugins")
 
 local plugins = {
     base_code_plugins,
